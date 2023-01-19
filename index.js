dd8638fad2b94c7aaa7a76251cfcbfcc5827aea6
Aleksandersie <aleksander.sie@gmail.com>
2023-01-19 23:37:43 +0300

    google shop

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 
 
 (async()=>{
-    const link = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=stepper+motor&_sacat=0&LH_TitleDesc=0&_odkw=stepper+motor&_osacat=0"
+
+    const selector = '.i0X6df'
+    const link = 'https://www.google.com/search?tbm=shop&hl=ru&psb=1&ved=2ahUKEwiY6bOMl9T8AhUaHwYAHbQ_CfwQu-kFegQIABAK&q=+6ES72111AE400XB0&oq=+6ES72111AE400XB0&gs_lcp=Cgtwcm9kdWN0cy1jYxADUABYAGAAaABwAHgAgAEAiAEAkgEAmAEA&sclient=products-cc'
     try{
         const browser = await puppeteer.launch({
             headless:false,
@@ -11,12 +13,34 @@ const puppeteer = require('puppeteer');
         })
         const page = await browser.newPage()
         await page.goto(link)
-        await page.waitForTimeout(5000)
-        const html = await page.evaluate(async ()=>{
-            const res = document.querySelectorAll('li.s-item__pl-on-bottom')
-            console.log(res)
+        await page.waitForSelector('h3.tAxDx')
+
+        const html = await page.evaluate( ()=>{
+            const res = []
+            try{
+                const card = document.querySelectorAll(".i0X6df")
+                card.forEach(el=>{
+                    const title = el.querySelector("h3.tAxDx")
+                    const price = el.querySelector("span.a8Pemb")
+                    const supplier = el.querySelector("div.aULzUe")
+
+                    const obj ={
+                        title: title.innerText,
+                        price: price.innerText,
+                        supplier: supplier.innerHTML
+                    }
+                    res.push(obj)
+                    console.log(obj)
+                })
+                console.log(res)
+            }catch (e){
+                console.log(e)
+            }
+            return res
         })
+        console.log(html)
     }catch (e){
         console.log(e)
     }
+
 })()
