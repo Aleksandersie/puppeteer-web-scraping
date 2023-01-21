b020e46ddb114c59db205e370f506422925abefd
Aleksandersie <aleksander.sie@gmail.com>
2023-01-21 11:08:00 +0300

    google shop

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const puppeteer = require('puppeteer');
 (async()=>{
 
     const selector = '.i0X6df'
+
     const link = 'https://www.google.com/search?tbm=shop&hl=ru&psb=1&ved=2ahUKEwiY6bOMl9T8AhUaHwYAHbQ_CfwQu-kFegQIABAK&q=+6ES72111AE400XB0&oq=+6ES72111AE400XB0&gs_lcp=Cgtwcm9kdWN0cy1jYxADUABYAGAAaABwAHgAgAEAiAEAkgEAmAEA&sclient=products-cc'
+    const linkTwo ="https://www.google.com/search?q=simotics+m+built-in&hl=ru&tbm=shop&sxsrf=AJOqlzVK2rL7X_SeuI6OSyhX-Wxjncu7iw%3A1674161179939&psb=1&ei=G6zJY6rYONKBi-gP_JOR0Ak&ved=0ahUKEwjq0bSzwNT8AhXSwAIHHfxJBJoQ4dUDCAg&oq=simotics+m+built-in&gs_lcp=Cgtwcm9kdWN0cy1jYxAMSgQIQRgAUABYAGAAaABwAHgAgAEAiAEAkgEAmAEA&sclient=products-cc"
     try{
         const browser = await puppeteer.launch({
             headless:false,
@@ -12,7 +14,7 @@ const puppeteer = require('puppeteer');
             devtools: true
         })
         const page = await browser.newPage()
-        await page.goto(link)
+        await page.goto(linkTwo)
         await page.waitForSelector('h3.tAxDx')
 
         const html = await page.evaluate( ()=>{
